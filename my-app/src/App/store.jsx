fa839bda22a614e925ceb2887980735a1f944e4c
wishromo <hinasaeed1212@gmail.com>
2025-05-30 13:02:14 +0000

    move store to App folder

---
+++ my-app/src/App/store.jsx
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import counterReducer from './redux/slice/counterSlice';
+ import authReducer from './redux/slice/authentication';
+
+
+export const store = configureStore(
+{reducer:{
+counter: counterReducer,
+ authentication: authReducer
+}}
+);
